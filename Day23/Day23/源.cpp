dddb0b2bcea3a5ed28dd55bbec31efa177dbc005
Be-doing <43647498+Be-doing@users.noreply.github.com>
2019-08-14 21:07:54 +0800

    微信红包

--- Day23/Day23/源.cpp
+++ Day23/Day23/源.cpp
@@ -1,68 +1,36 @@
-//#include<iostream>
-//#include<vector>
-//#include<unordered_map>
-//#include<algorithm>
-//using namespace std;
-//class Gift {
-//public:
-//	int getValue(vector<int> gifts, int n) {
-//		// write code here
-//		//ÕÒÊı×éÖĞ³öÏÖÁËÒ»°ëµÄÊı
-//		//2    2    2    3    3
-//		//1    1    2    2    2
-//		unordered_map<int, int> m;
-//		for (int i = 0; i < n; ++i)
-//		{
-//			++m[gifts[i]];
-//		}
-//		for (const auto&e : m)
-//		{
-//			if (e.second > (gifts.size() / 2))
-//			{
-//				return e.first;
-//			}
-//		}
-//		return 0;
-//	}
-//};
-//
-//int main()
-//{
-//	Gift g;
-//	vector<int> v = {1,1,2};
-//	cout << g.getValue(v, v.size()) << endl;
-//	system("pause");
-//	return 0;
-//}
-
-
 #include<iostream>
-#include<string>
+#include<vector>
+#include<unordered_map>
+#include<algorithm>
 using namespace std;
-void Solution(const string& str1, const string& str2)
-{
-	int count = 0;
-	count = str1.size() > str2.size() ? str1.size() - str2.size() : str2.size() - str1.size();
-	for (int i = 0; i < str2.size(); ++i)
-	{
-		if (i == str1.size())
+class Gift {
+public:
+	int getValue(vector<int> gifts, int n) {
+		// write code here
+		//æ‰¾æ•°ç»„ä¸­å‡ºç°äº†ä¸€åŠçš„æ•°
+		//2    2    2    3    3
+		//1    1    2    2    2
+		unordered_map<int, int> m;
+		for (int i = 0; i < n; ++i)
 		{
-			break;
+			++m[gifts[i]];
 		}
-		if (str1[i] != str2[i])
+		for (const auto&e : m)
 		{
-			++count;
+			if (e.second > (gifts.size() / 2))
+			{
+				return e.first;
+			}
 		}
+		return 0;
 	}
-	cout << count << endl;
-}
+};
+
 int main()
 {
-	string str1;
-	string str2;
-	while (cin >> str1 >> str2)
-	{
-		Solution(str1, str2);
-	}
+	Gift g;
+	vector<int> v = {1,1,2};
+	cout << g.getValue(v, v.size()) << endl;
+	system("pause");
 	return 0;
 }
