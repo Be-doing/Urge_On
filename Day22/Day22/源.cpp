59b4d78f479a274162a97b641f77e91514a00545
Be-doing <43647498+Be-doing@users.noreply.github.com>
2019-08-14 21:04:05 +0800

    小易的升级之路&&找出字符串中第一个只出现一次的字符

--- Day22/Day22/源.cpp
+++ Day22/Day22/源.cpp
@@ -1,36 +1,79 @@
+#include<iostream> 
+using namespace std; 
+int ConventionNo(const int& a, const int& b) 
+{ 
+	for(int i = a; i > 0; --i) 
+	{ 
+		if(b % i == 0 && a % i == 0) 
+		{ 
+			return i; 
+		} 
+	}
+	return 0; 
+}
+void Solution(const int& n, int& a) 
+{ 
+	int data;//怪物数据 
+	int* arr = new int[n]; 
+	for(int i = 0; i < n; ++i) 
+	{ 
+		cin >> data; 
+		arr[i] = data; 
+	}
+	for(int i = 0; i < n; ++i)//开始打怪 
+	{ 
+		if(a >= arr[i]) 
+		{ 
+			a += arr[i]; 
+		}
+		else 
+		{ 
+			a += ConventionNo(a,arr[i]); 
+		} 
+	}
+	cout << a << endl; 
+}
+int main() 
+{ 
+	int n;//怪物数量 
+	int a;//初始值 
+	while(cin >> n >> a) 
+	{ 
+		Solution(n,a); 
+	}
+	return 0; 
+}
+
+
+
+
+
 #include<iostream>
 #include<string>
-#include<unordered_map>
-
+#include<map>
 using namespace std;
-void Solution(string& str)
-{
-	unordered_map<char, int> m;
-	for (int i = 0; i < str.size(); ++i)
-	{
-		++m[str[i]];
-	}
-	int flag = 1;
-	for (auto& e : m)
-	{
-		if (e.second == 1)
-		{
-			cout << e.first << endl;
-			flag = 0;
-			break;
-		}
-	}
-	if (flag == 1)
-	{
-		cout << -1 << endl;
-	}
-}
+
 int main()
 {
-	string str;
-	while (cin >> str)
-	{
-		Solution(str);
-	}
-	return 0;
+    string str;
+
+    while(cin >> str)
+    {
+       int flag = 0;
+        map<char, int> m;
+        for(const auto& c : str)
+            ++m[c];
+        for(const auto& c : str)
+        {
+            if(m[c] == 1)
+            {
+                cout << c << endl;
+                ++flag;
+                break;
+            }
+        }
+    if(flag == 0)
+        cout << -1 << endl;
+    }
+    return 0;
 }
